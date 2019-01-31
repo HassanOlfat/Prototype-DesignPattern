8aa924209b752f19c84b6ca31e96a618f1af8422
hassan olfat <hassan.olfat@LIVE.COM>
2019-01-31 12:41:29 +0330

    Add Class and Student Classes

---
+++ Prototype/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    internal abstract class Class
+    {
+        internal int Id { get; set; }
+        internal string Name { get; set; }
+        internal int Capacity { get; set; }
+
+    }
+}
